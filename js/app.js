7b2f4b3e6148d4d7ba9780bafceb26f9f250a244
bencohen268 <150835863+bencohen268@users.noreply.github.com>
2023-11-14 12:13:57 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+(()=>{"use strict";var __webpack_require__={};(()=>{__webpack_require__.r=(exports)=>{if(typeof Symbol!=='undefined'&&Symbol.toStringTag){Object.defineProperty(exports,Symbol.toStringTag,{value:'Module'});}
+Object.defineProperty(exports,'__esModule',{value:true});};})();var __webpack_exports__={};/*!*****************************!*\
+!*** ./resources/js/app.js ***!
+\*****************************/__webpack_require__.r(__webpack_exports__);Object(function webpackMissingModule(){var e=new Error("Cannot find module 'vue'");e.code='MODULE_NOT_FOUND';throw e;}());})();
